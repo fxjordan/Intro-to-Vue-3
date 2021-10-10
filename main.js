461c9ec4d4e43b9665e328303de79c8bd6f3e877
Felix Jordan <felix.jordan@buddy-app.de>
2021-10-10 20:26:07 +0200

    L2 my solution

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Socks are very usefuly things that humans like to use on specific parts of their body.'
+        };
+    }
+});
